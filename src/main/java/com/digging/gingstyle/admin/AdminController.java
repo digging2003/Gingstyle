626684dc6bf213af7efbf172815174776f7d7651
digging <digging@gmail>
2025-05-23 20:58:57 +0900

    [이지원] - 관리자 페이지 정리

--- src/main/java/com/digging/gingstyle/admin/AdminController.java
+++ src/main/java/com/digging/gingstyle/admin/AdminController.java
@@ -1,22 +1,38 @@
 package com.digging.gingstyle.admin;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.digging.gingstyle.products.dto.Detail;
+import com.digging.gingstyle.products.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor // final이 부여된 멤버변수만
 @RequestMapping("/admin")
 @Controller
 public class AdminController {
+	
+	private final ProductService productService;
 
 	// 대시보드
 	@GetMapping
-	public String dashboard() {
+	public String dashboard(Model model) {
 		return "admin/dashboard";
 	}
 	
 	// 상품 관리
 	@GetMapping("/product")
-	public String product() {
+	public String product(Model model) {
+		List<Detail> detailList = productService.getCategoryView(null);
+		
+		model.addAttribute("detailList", detailList);
+		
 		return "admin/product";
 	}
 	
@@ -32,5 +48,20 @@ public class AdminController {
 		return "admin/order";
 	}
 	
+	// 사용자 관련 - 주문 진행 화면
+//	@PostMapping
+//	public String orderView(
+//			@RequestParam("cartId") List<Integer> cartIdList
+//			, Model model
+//			, HttpSession session) {
+//		
+//		int userId = (Integer) session.getAttribute("userId");
+//		
+//		List<CartView> cartViewList = orderService.getOrderItemViewList(cartIdList, userId);
+//		
+//		model.addAttribute("cartViewList", cartViewList);
+//		
+//		return "user/order";
+//	}
 	
 }
